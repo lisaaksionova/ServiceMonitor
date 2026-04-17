43b7ab1334bcb17e3a131f647e503a4acf39fc8e
Yelyzaveta Aksionova <lisaaksionova@gmail.com>
2026-04-18 01:42:44 +0300

    add service collection extensions

--- ServiceMonitor.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ ServiceMonitor.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ServiceMonitor.Domain.Interfaces;
 using ServiceMonitor.Infrastructure.Persistence;
+using ServiceMonitor.Infrastructure.Repositories;
 
 namespace ServiceMonitor.Infrastructure.Extensions;
 
-public static class ServiceCollectionExtensions
+public static class ServiceCollectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("MonitorDatabase");
         services.AddDbContext<MonitorDbContext>(options => options.UseNpgsql(connectionString));
+
+        services.AddScoped<IServiceRepository, ServiceRepository>();
     }
 }

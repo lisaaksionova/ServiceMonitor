644b3c5b45eb62dd9e709f9d43c24195fb467f22
Yelyzaveta Aksionova <lisaaksionova@gmail.com>
2026-05-18 22:50:14 +0300

    add auth controller

---
+++ ServiceMonitor.API/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ServiceMonitor.Application.Auth.LoginUser;
+using ServiceMonitor.Application.Auth.RegisterUser;
+
+namespace ServiceMonitor.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login([FromBody] LoginUserCommand command)
+    {
+        var token = await mediator.Send(command);
+        
+        return Ok("ACCESS_TOKEN: " + token);
+    }
+    
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserCommand command)
+    {
+        await mediator.Send(command);
+        
+        return Created();
+    }
+}

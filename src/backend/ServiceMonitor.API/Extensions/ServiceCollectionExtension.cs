35725e386c47029ec4922d58b4a444477e24351e
Yelyzaveta Aksionova <lisaaksionova@gmail.com>
2026-08-11 12:24:37 +0300

    enhance service extention

--- src/backend/ServiceMonitor.API/Extensions/ServiceCollectionExtension.cs
+++ src/backend/ServiceMonitor.API/Extensions/ServiceCollectionExtension.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
 using ServiceMonitor.API.Middlewares;
 using ServiceMonitor.Domain.Entities;
@@ -32,5 +35,47 @@ public static class ServiceCollectionExtension
             });
         });
         services.AddHttpContextAccessor();
+    }
+
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+    }
+
+    public static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(opts =>
+    {
+        opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        opts.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(opts =>
+    {
+        opts.SaveToken = true;
+        opts.RequireHttpsMetadata = false;
+
+        opts.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = configuration["JWT:ValidAudience"],
+            ValidIssuer = configuration["JWT:ValidIssuer"],
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    configuration["JWT:Secret"]!
+                )
+            )
+        };
+    });
     }
 }
--- src/backend/ServiceMonitor.API/Program.cs
+++ src/backend/ServiceMonitor.API/Program.cs
@@ -1,6 +1,3 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using ServiceMonitor.API.Extensions;
 using ServiceMonitor.API.Middlewares;
@@ -10,40 +7,17 @@ using ServiceMonitor.Infrastructure.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddPresentation();
 
-builder.Services.AddAuthentication(opts =>
-    {
-        opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        opts.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(opts =>
-    {
-        opts.SaveToken = true;
-        opts.RequireHttpsMetadata = false;
-
-        opts.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["JWT:ValidAudience"],
-            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-            ClockSkew = TimeSpan.Zero,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(
-                    builder.Configuration["JWT:Secret"]!
-                )
-            )
-        };
-    });
+builder.Services.ConfigureCors();
+builder.Services.ConfigureAuthentication(builder.Configuration);
 
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 

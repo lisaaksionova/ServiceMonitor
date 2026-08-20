012331c38a6ef2df361cfe34f35f18d8ce1dbfbe
Yelyzaveta Aksionova <lisaaksionova@gmail.com>
2026-08-21 00:24:10 +0300

    make field nullable

---
+++ src/backend/ServiceMonitor.Infrastructure/Migrations/20260820205006_Nullable_LastFailureReason.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceMonitor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nullable_LastFailureReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastFailureReason",
+                table: "Services",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastFailureReason",
+                table: "Services",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}

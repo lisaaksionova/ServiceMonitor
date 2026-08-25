f68be4c5894134f4f957cfefd9a40f2ee48b9b31
lisaaksionova <lisaaksionova@gmail.com>
2026-08-25 17:49:27 +0300

    add auth validators

---
+++ src/backend/ServiceMonitor.Application/Auth/LoginUser/LoginUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace ServiceMonitor.Application.Auth.LoginUser;
+
+public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+{
+    public LoginUserCommandValidator()
+    {
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .WithMessage("Email is required");
+
+        RuleFor(u => u.Password)
+            .NotEmpty()
+            .WithMessage("Password is required");
+    }
+}

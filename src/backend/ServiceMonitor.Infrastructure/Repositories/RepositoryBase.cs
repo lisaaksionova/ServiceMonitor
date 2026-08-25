a8c78adcecf79cf910a9b7e03106fe3f24cf0792
lisaaksionova <lisaaksionova@gmail.com>
2026-08-25 11:24:28 +0300

    change tracking mechanism

--- src/backend/ServiceMonitor.Infrastructure/Repositories/RepositoryBase.cs
+++ src/backend/ServiceMonitor.Infrastructure/Repositories/RepositoryBase.cs
@@ -16,8 +16,8 @@ public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
     public void Create(T entity) => _context.Set<T>().Add(entity);
     public void Delete(T entity) => _context.Set<T>().Remove(entity);
     public IQueryable<T> GetAll() =>
-      _context.Set<T>().AsNoTrackingWithIdentityResolution();
+      _context.Set<T>().AsNoTracking();
     public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression) =>
-      _context.Set<T>().Where(expression).AsNoTrackingWithIdentityResolution();
+      _context.Set<T>().Where(expression).AsNoTracking();
     public void Update(T entity) => _context.Set<T>().Update(entity);
 }

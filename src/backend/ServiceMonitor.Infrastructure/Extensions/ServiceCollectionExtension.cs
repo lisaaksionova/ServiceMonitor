f748186498e2ef18744a993add01aeb2160e0ff9
Yelyzaveta Aksionova <135411053+lisaaksionova@users.noreply.github.com>
2026-09-05 00:37:34 +0300

    Feature/ef core (#20)
    * add order-by
    
    * refactor
    
    * fix references

--- src/backend/ServiceMonitor.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ src/backend/ServiceMonitor.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using ServiceMonitor.Domain.Interfaces;
 using ServiceMonitor.Infrastructure.BackgroundServices;
 using ServiceMonitor.Infrastructure.Persistence;
-using ServiceMonitor.Infrastructure.Seeders;
+using ServiceMonitor.Infrastructure.Repositories;
 
 namespace ServiceMonitor.Infrastructure.Extensions;
 
@@ -13,9 +13,10 @@ public static class ServiceCollectionExtension
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("MonitorDatabase");
-        services.AddDbContextPool<MonitorDbContext>(options => options.UseNpgsql(connectionString));
+        services.AddDbContext<MonitorDbContext>(options => options.UseNpgsql(connectionString));
 
         services.AddScoped<IRepositoryManager, RepositoryManager>();
+
         services.AddHttpClient();
         services.AddHostedService<HealthCheckBackgroundService>();
     }

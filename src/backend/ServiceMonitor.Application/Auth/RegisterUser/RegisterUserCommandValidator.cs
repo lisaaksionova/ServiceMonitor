7d0014761dda68b4062643830b070f540962d074
Yelyzaveta Aksionova <135411053+lisaaksionova@users.noreply.github.com>
2026-09-05 00:27:27 +0300

    Feature/security identity (#19)
    * add auth validators
    
    * update jwt settings

---
+++ src/backend/ServiceMonitor.Application/Auth/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace ServiceMonitor.Application.Auth.RegisterUser;
+
+public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .WithMessage("Email is required");
+
+        RuleFor(u => u.Password)
+            .NotEmpty()
+            .WithMessage("Password is required");
+    }
+}
